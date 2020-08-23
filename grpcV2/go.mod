0cd55a10c1c07da02f78868ebc22557b3568bbb0
Nicolas Riquelme <nrriquelme@gmail.com>
2020-08-22 22:40:20 -0400

    grpc example from docu

--- grpcV2/go.mod
+++ grpcV2/go.mod
@@ -2,4 +2,9 @@ module github.com/wainola/proglog/grpcV2
 
 go 1.15
 
-require github.com/gogo/protobuf v1.3.1 // indirect
+require (
+	github.com/gogo/protobuf v1.3.1
+	github.com/golang/protobuf v1.4.2
+	google.golang.org/grpc v1.31.0
+	google.golang.org/grpc/examples v0.0.0-20200822010404-0e72e09474d6
+)
---
+++ grpcV2/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"github.com/wainola/proglog/grpcV2/data"
+	pb "github.com/wainola/proglog/grpcV2/routeguide"
+	"github.com/wainola/proglog/grpcV2/server"
+)
+
+var (
+	tls      = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	certFile = flag.String("cert_file", "", "The TLS cert file")
+	keyFile  = flag.String("key_file", "", "The TLS key file")
+	port     = flag.Int("port", 10000, "The server port")
+)
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	if *tls {
+		if *certFile == "" {
+			*certFile = data.Path("x509/server_cert.pem")
+		}
+		if *keyFile == "" {
+			*keyFile = data.Path("x509/server_key.pem")
+		}
+		creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+		if err != nil {
+			log.Fatalf("Failed to generate credentials %v", err)
+		}
+		opts = []grpc.ServerOption{grpc.Creds(creds)}
+	}
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterRouteGuideServer(grpcServer, server.NewServer())
+	grpcServer.Serve(lis)
+}
